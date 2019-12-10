f157174ddc8794f1e239e79e4833121641cfb7fc
unalo <unalomtshokotsha@gmail.com>
2019-12-10 11:58:14 +0200

    added pages, need to fix a small bug on wifi page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,9 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import static spark.Spark.*;
 
 public class App {
@@ -22,10 +20,25 @@ public class App {
         staticFiles.location("/public");
 
         port(getHerokuAssignedPort());
+        Map<String, Object> dataMap = new HashMap<>();
+
 
         get("/", (req, res) -> {
             return new ModelAndView(new HashMap<>(), "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/Home", (req,res) -> {
+            String name = req.queryParams("firstName").substring(0,1).toUpperCase() + req.queryParams("firstName").substring(1).toLowerCase();
+            dataMap.put("userName", name);
+            return new ModelAndView(dataMap, "home.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/Wifi", (req,res) -> {
+            return new ModelAndView(new HashMap<>(), "hotspots.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
+
+
     
 }
