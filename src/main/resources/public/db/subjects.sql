19e4bd722341ecda395045a361655c7c820759e3
Sandiso Madayi <sandisomadayi239@gmail.com>
2020-03-05 09:39:45 +0200

    Update subjects.sql

--- src/main/resources/public/db/subjects.sql
+++ src/main/resources/public/db/subjects.sql
@@ -2,6 +2,3 @@ create table subjects (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     subject_name text;
 );
-insert into subjects(subject_name) values ('Mathematics');
-insert into subjects(subject_name) values ('Physical Sciences');
-insert into subjects(subject_name) values ('Life Sciences');
